05a3faebabcb68a8293c2c951ec7780b41428e2e
Abdallah Awwad <abawad@uplandsoftware.com>
2020-05-02 12:17:50 +0200

    expense manager route navigation

---
+++ src/components/global/Navbar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { arrayOf, shape, string } from "prop-types";
+import styles from "./Navbar.module.css";
+
+const Navbar = ({ routes }) => (
+  <div className={styles.navbar}>
+    {routes.map((route) => (
+      <NavLink
+        className={styles.navLink}
+        activeClassName={styles.navLinkActive}
+        exact
+        to={route.url}
+      >
+        {route.text}
+      </NavLink>
+    ))}
+  </div>
+);
+
+const routeShape = shape({
+  text: string.isRequired,
+  url: string.isRequired,
+});
+
+Navbar.propTypes = {
+  routes: arrayOf(routeShape),
+};
+
+export default Navbar;
--- src/routes/ExpenseManager/index.js
+++ src/routes/ExpenseManager/index.js
@@ -1,15 +1,24 @@
 import React, { Suspense, lazy } from "react";
 import { Route, Switch } from "react-router-dom";
+
 import Loader from "components/global/Loader";
+import Navbar from "components/global/Navbar";
+
+import styles from "./index.module.css";
 
 const Overview = lazy(() => import("./Overview"));
 const MyReports = lazy(() => import("./MyReports"));
 
 const ExpenseManger = () => {
+  const routes = [
+    {text: "Overview", url: "/"},
+    {text: "My Reports", url: "/my-reports"},
+  ]
 
   return(
   <section>
-    <h2>Expense Report Manager</h2>
+    <h2 className={styles.title}>Expense Report Manager</h2>
+    <Navbar routes={routes} />
       <Suspense fallback={<Loader />}>
         <Switch>
           <Route exact path="/" component={Overview} />
